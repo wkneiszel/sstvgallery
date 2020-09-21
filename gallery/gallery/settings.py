afd2ad7177f2953100ec6022767ca781a1c1ee3d
wkneisze <wilson@kneiszel.com>
2020-09-21 12:42:36 -0400

    created basic models for Image and Comment

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sstvgallery.apps.SstvgalleryConfig'
 ]
 
 MIDDLEWARE = [
--- gallery/sstvgallery/models.py
+++ gallery/sstvgallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    photo = models.ImageField('SSTV Image')
+    receive_date = models.DateTimeField('Date Received')
+
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    comment_text = models.TextField('Comment Text')
+    comment_date = models.DateTimeField('Date Posted')
